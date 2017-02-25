05874f4add6b31222b12b55667b7f9d022b7d799
mohammedashiqu <ashiqit93@gmail.com>
2017-02-25 14:20:30 +0530

    menu print

---
+++ calc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+
+int c;
+do
+  {
+   printf("\nclaculator");
+   printf("\n1:addition\n2:substraction\n3:multiplication\n4:division\n5:exit");
+   printf("\nenter your choice");
+   scanf("%d",&c);
+  }while(c!=5);
+}
